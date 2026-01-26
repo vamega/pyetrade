46de096bf805ca73d76d5a12b45888637b2f378d
Varun Madiath <github@madiathv.com>
2026-01-26 17:17:24 -0500

    feat: Export OrderBuilder from pyetrade package, bump to v2.2.0

--- pyetrade/__init__.py
+++ pyetrade/__init__.py
@@ -1,5 +1,5 @@
-"""Init for pyetrade module  """
-__version__ = "2.1.1"
+"""Init for pyetrade module"""
+__version__ = "2.2.0"
 
 from . import authorization  # noqa: F401
 from .authorization import ETradeOAuth, ETradeAccessManager  # noqa: F401
@@ -11,3 +11,7 @@ from . import order  # noqa: F401
 from .order import ETradeOrder  # noqa: F401
 from . import alerts  # noqa: F401
 from .alerts import ETradeAlerts  # noqa: F401
+
+# New modules for enhanced functionality
+from . import types  # noqa: F401
+from .order_builder import OrderBuilder, OrderBuilderError  # noqa: F401
